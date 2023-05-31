1ff66c3be4589f0fd8f01772171f27bf078b71c7
mollimoon <zettai.kareshi@yandex.ru>
2023-05-31 11:34:17 +0300

    created post states

---
+++ lib/features/post/bloc/post_state.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:study_architecture/data/post.dart';
+
+class PostState {}
+
+class PostLoadingState extends PostState with EquatableMixin {
+  @override
+  List<Object?> get props => [];
+}
+
+class PostSuccessState extends PostState with EquatableMixin {
+  final List<Post> postList;
+  PostSuccessState({required this.postList});
+
+  @override
+  List<Object?> get props => [];
+}
+
+class PostErrorState extends PostState with EquatableMixin {
+  final String errorText;
+  PostErrorState({required this.errorText});
+
+  @override
+  List<Object?> get props => [];
+}
