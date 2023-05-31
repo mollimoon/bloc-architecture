4b44933a1da323ef7037bf420cc92cad88fd9def
mollimoon <zettai.kareshi@yandex.ru>
2023-05-31 12:06:21 +0300

    added post cubit

---
+++ lib/features/post/bloc/post_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:study_architecture/data/post_repository.dart';
+import 'package:study_architecture/features/post/bloc/post_state.dart';
+
+class PostCubit extends Cubit<PostState> {
+  PostCubit(super.initialState);
+
+  final _postRepository = PostRepository(dio: Dio());
+
+  Future<void> fetchPostList() async {
+    emit(PostLoadingState());
+
+    final postList = await _postRepository.getPosts();
+
+    emit(PostSuccessState(postList: postList));
+  }
+}
